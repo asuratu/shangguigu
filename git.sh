34feaa4ea149d58e2f6f88f708b445abc8c99bf5
AsuraTu <tugecn@gmail.com>
2023-09-21 16:36:44 +0800

    update

--- git.sh
+++ git.sh
@@ -5,6 +5,12 @@ if [ -z "$CONTENT" ]; then
     CONTENT="update"
 fi
 
+git pull
+echo "git pull success"
+
 git add .
 git commit -m "$CONTENT"
+
+# git push
 git push origin main
+echo "git push success"
